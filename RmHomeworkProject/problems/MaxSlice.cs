7170e6b091095d5877035eb33feb6414d2ecfff0
Rupal Majmudar <rupal.majmudar@bob.ch>
2021-04-13 00:18:01 +0200

    sliced

---
+++ RmHomeworkProject/problems/MaxSlice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RmHomeworkProject.problems {
+    class MaxSlice {
+        public int Solution(int[] A) {
+            int[] B = GetCumulativeOf(A);
+            int maxSum = 0;
+            int sum = 0;
+            for (int p = 0; p < A.Length; p++) {
+                for (int q = p+1; q < A.Length; q++) {
+                    sum = B[q] - B[p];
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                    }
+                }
+            }
+
+            return maxSum;
+        }
+
+        private int[] GetCumulativeOf(int[] A) {
+            int[] B = new int[A.Length + 1];
+            B[0] = 0; ;
+            for (int i=1; i < A.Length+1; i++) {
+                B[i] = B[i - 1] + A[i - 1];
+            }
+            return B;
+        }
+    }
+}
---
+++ RmHomeworkProject/tests/problems/MaxSliceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+using RmHomeworkProject.problems;
+
+namespace RmHomeworkProject.tests.problems {
+    class MaxSliceTest {
+
+        [Test]
+        public void Test() {
+            int[] A = { 5, -7, 3, 5, -2, 4, -1 };
+            MaxSlice slice = new MaxSlice();
+            int maxSum = slice.Solution(A);
+            Assert.AreEqual(10, maxSum);
+
+            int[] A2 = { 3, 2, -6, 4, 0 };
+            int maxSum2 = slice.Solution(A2);
+            Assert.AreEqual(5, maxSum2);
+        }
+    }
+}
