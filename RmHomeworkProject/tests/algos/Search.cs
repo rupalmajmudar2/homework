a5b65f6f27e78c2d82f88e5ba763d2d96718f320
Rupal Majmudar <rupal.majmudar@bob.ch>
2022-06-05 20:36:35 +0200

    fixes

---
+++ RmHomeworkProject/tests/algos/Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace RmHomeworkProject.tests.algos {
+    class Search_Binary {
+        int[] int_arr;
+
+        [Test]
+        public void TestBinarySearch() {
+            Search_Binary search = new Search_Binary();
+            search.int_arr = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 }; //sorted!
+            int toFind = 4;
+            int location = search.binarySearch(0, search.int_arr.Length, toFind);
+            Debug.WriteLine("Element " + toFind + " is at position#" + location);
+            Assert.AreEqual(location, 3);
+        }
+
+        int binarySearch(int low, int high, int key) {
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (int_arr[mid] < key) {
+                    low = mid + 1;
+                }
+                else if (int_arr[mid] > key) {
+                    high = mid - 1;
+                }
+                else {
+                    return mid;
+                }
+            }
+            return -1;                //key not found
+        }
+    }
+}
---
+++ RmHomeworkProject/tests/algos/Sort.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace RmHomeworkProject.algos.sort {
+    public class Sort {
+        [Test]
+        public void TestBubbleSort() {
+            Sort sort = new Sort();
+            int[] toSort = { 7, 4, 5, 2 };
+
+            int[] bubbleSorted = sort.bubble_sort(toSort);
+            Assert.IsNotNull(bubbleSorted);
+        }
+
+        public int[] bubble_sort(int[] A) {
+            int n = A.Length;
+            int temp;
+            for (int k = 0; k < n - 1; k++) {
+                // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
+
+                for (int i = 0; i < n - k - 1; i++) {
+                    if (A[i] > A[i + 1]) {
+                        // here swapping of positions is being done.
+                        temp = A[i];
+                        A[i] = A[i + 1];
+                        A[i + 1] = temp;
+                    }
+                }
+            }
+
+            return A;
+        }
+    }
+}
